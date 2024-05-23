82c478f081ab7887d42eedbc36e8ef64205b4ed4
Landry N. <landry@Landrys-MacBook-Air.local>
2024-05-23 20:01:56 +0100

    final changes

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -55,7 +55,7 @@ export const paymentRouter = router({
       })
 
       line_items.push({
-        price: 'price_1OCeBwA19umTXGu8s4p2G3aX',
+        price: 'price_1PJfKyI5yRrAyZf1FHfv9MhA',
         quantity: 1,
         adjustable_quantity: {
           enabled: false,
