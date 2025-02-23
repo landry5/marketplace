01f4495b8a4887c44fdfbb608d977d9438a49f96
landry5 <nsengilandry5@gmail.com>
2025-02-23 20:32:33 +0000

    final product delivery email fix. fixed stripe webhooks, and updated custom email domain to noreply-marketplace@landryn.dev

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -99,8 +99,8 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: 'onboarding@resend.dev',
-        //from: 'DigitalMarketplace <digitalkit@landryn.dev>',
+        //from: 'onboarding@resend.dev',
+        from: 'DigitalMarketplace <noreply-marketplace@landryn.dev>',
         to: [user.email],
         subject:
           'Thanks for your order! This is your receipt.',
