bd70df5a4db0f6b79cac59ed4b60c2383ab55e5e
landry5 <nsengilandry5@gmail.com>
2025-02-25 10:20:29 +0000

    changed upload media types to alow zip files

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -64,7 +64,7 @@ export const Media: CollectionConfig = {
         position: 'centre',
       },
     ],
-    mimeTypes: ['image/*'],
+    mimeTypes: ['image/*', 'application/zip'],
   },
   fields: [
     {
